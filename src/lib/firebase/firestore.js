0810967c7c4f2028911c3cd96bd77796667d3470
Erik <erikwpond@gmail.com>
2025-10-14 22:01:08 -0700

    Allow users to submit restaurant reviews

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -32,20 +32,61 @@ export async function updateRestaurantImageReference(
   }
 }
 
-// Placeholder function for updating restaurant rating in a transaction
+// Function for updating restaurant rating
 const updateWithRating = async (
   transaction,
   docRef,
   newRatingDocument,
   review
 ) => {
-  return;
+  const restaurant = await transaction.get(docRef);
+  const data = restaurant.data();
+  const newNumRatings = data?.numRatings ? data.numRatings + 1 : 1;
+  const newSumRating = (data?.sumRating || 0) + Number(review.rating);
+  const newAverage = newSumRating / newNumRatings;
+
+  transaction.update(docRef, {
+    numRatings: newNumRatings,
+    sumRating: newSumRating,
+    avgRating: newAverage,
+  });
+
+  transaction.set(newRatingDocument, {
+    ...review,
+    timestamp: Timestamp.fromDate(new Date()),
+  });
 };
 
-// Placeholder for adding a review to a restaurant using a transaction.
+
+// Add a review to a restaurant
 export async function addReviewToRestaurant(db, restaurantId, review) {
-  return;
+        if (!restaurantId) {
+                throw new Error("No restaurant ID has been provided.");
+        }
+
+        if (!review) {
+                throw new Error("A valid review has not been provided.");
+        }
+
+        try {
+                const docRef = doc(collection(db, "restaurants"), restaurantId);
+                const newRatingDocument = doc(
+                        collection(db, `restaurants/${restaurantId}/ratings`)
+                );
+
+                // corrected line
+                await runTransaction(db, transaction =>
+                        updateWithRating(transaction, docRef, newRatingDocument, review)
+                );
+        } catch (error) {
+                console.error(
+                        "There was an error adding the rating to the restaurant",
+                        error
+                );
+                throw error;
+        }
 }
+
 
 // Applies Firestore query filters based on user-selected search params.
 function applyQueryFilters(q, { category, city, price, sort }) {
